4ff56936ce9271a80302bd9ae1dd7332daeaf3be
Janik Schumacher <rndmb0t@gmail.com>
2019-11-05 23:13:52 +0100

    Used new static extentions for static effect

--- LedController/LedController/Effects/StaticEffect.cs
+++ LedController/LedController/Effects/StaticEffect.cs
@@ -6,21 +6,15 @@ namespace LedController.Models.Effects
 {
     public class StaticEffect : LedEffect
     {
-        private RgbColor _color;
+        private readonly RgbColor _color;
         public StaticEffect(int ledCount, string name, RgbColor color): base(ledCount, name)
         {
             _color = color;
         }
         public override byte[] Render()
         {
-            var a = new List<byte>();
-            for (int i = 0; i < LedCount; i++)
-            {
-                a.Add((byte)_color.R);
-                a.Add((byte)_color.G);
-                a.Add((byte)_color.B);
-            }
-            return a.ToArray();
+            LEDs.SetAll(_color);
+            return LEDs.ToByteArray();
         }
     }
 }
