a59dadca9cda66b7f81a5c71091599388cb59450
Janik Schumacher <rndmb0t@gmail.com>
2019-11-20 18:27:46 +0100

    removed test code

--- WebApi/Awdware.Business.Facade/Controllers/TestController.cs
+++ WebApi/Awdware.Business.Facade/Controllers/TestController.cs
@@ -1,14 +1,11 @@
 ﻿using Awdware.Facade.Dtos;
-using Awdware.Facade.Led.Dtos;
-using Awdware.Facade.Led.Interfaces;
-using Awdware.Facade.Led.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Awdware.Business.Facade.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TestController : ControllerBase, ILedController
+    public class TestController : ControllerBase
     {
         public IActionResult Index()
         {
@@ -19,15 +16,6 @@ namespace Awdware.Business.Facade.Controllers
                 Username = "testUser"
             };
             return Ok(a);
-        }
-
-        [HttpGet]
-        [Route("Led/{ledCount}")]
-        public LedImageDto GetLedImage(uint ledCount)
-        {
-            var a = new LedImage(ledCount);
-            a.Leds.ForEach(x => x.SetColor(RgbColor.GetRandom()));
-            return a.ToDto();
         }
     }
 }
