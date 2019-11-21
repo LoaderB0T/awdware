d6c9b805d7947ae716fd4a6526d86c24281b0615
Janik Schumacher <rndmb0t@gmail.com>
2019-11-21 20:03:50 +0100

    AsyncLedController Interface

---
+++ WebApi/Awdware.Facade.Led/Interfaces/IAsyncLedController.cs
@@ -0,0 +1,10 @@
+﻿using Awdware.Facade.Led.Dtos;
+using System.Threading.Tasks;
+
+namespace Awdware.Facade.Led.Interfaces
+{
+    interface IAsyncLedController
+    {
+        public Task<LedImageDto> GetLedImage(uint ledCount);
+    }
+}
