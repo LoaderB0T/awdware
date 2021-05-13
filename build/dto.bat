9d2339126b51dac494809dee02b81370b6913350
Janik Schumacher <rndmb0t@gmail.com>
2021-05-13 16:17:18 +0200

    Fixed dto translator

--- build/dto.bat
+++ build/dto.bat
@@ -1,6 +1,6 @@
-start /B DtoTranslator.exe "D:\git\awdware\Src\Awdware.Blog\Awdware.Blog.Facade\obj\Debug\netcoreapp3.1\Awdware.Blog.Facade.dll" "D:\git\awdware\Src\Awdware.Blog\Awdware.Blog.Presentation\projects\awdware-blog\src\lib\models\application-facade.ts"
+start /B DtoTranslator.exe "D:\git\awdware\bin\Debug\net5.0\Awdware.Blog.Facade.dll" "D:\git\awdware\Src\Awdware.Blog\Awdware.Blog.Presentation\projects\awdware-blog\src\lib\models\application-facade.ts"
 
-start /B DtoTranslator.exe "D:\git\awdware\Src\Awdware.Core\Awdware.Core.Facade\obj\Debug\netcoreapp3.1\Awdware.Core.Facade.dll" "D:\git\awdware\Src\Awdware.Core\Awdware.Core.Presentation\projects\awdware-core\src\lib\models\application-facade.ts"
+start /B DtoTranslator.exe "D:\git\awdware\bin\Debug\net5.0\Awdware.Core.Facade.dll" "D:\git\awdware\Src\Awdware.Core\Awdware.Core.Presentation\projects\awdware-core\src\lib\models\application-facade.ts"
 
-start /B DtoTranslator.exe "D:\git\awdware\Src\Awdware.Led\Awdware.Led.Facade\obj\Debug\netcoreapp3.1\Awdware.Led.Facade.dll" "D:\git\awdware\Src\Awdware.Led\Awdware.Led\Awdware.Led.Presentation\projects\awdware-led\src\lib\models\application-facade.ts"
+start /B DtoTranslator.exe "D:\git\awdware\bin\Debug\net5.0\Awdware.Led.Facade.dll" "D:\git\awdware\Src\Awdware.Led\Awdware.Led.Presentation\projects\awdware-led\src\lib\models\application-facade.ts"
 pause
